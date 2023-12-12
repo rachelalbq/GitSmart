1495ff6f3a8fd98925fc8bc783395d6ebfdb8220
rachelalbq <rachel.alb.97@hotmail.com>
2023-12-12 12:04:59 -0300

    refactor: remove token

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  TOKEN: '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',
+  TOKEN: '',
 };
+// coloque aqui seu token
